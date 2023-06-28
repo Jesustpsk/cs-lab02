e489bd797826e1235c2bb00e55a769a6ca00f88f
Alice (IvanovII) <alice@example.com>
2023-06-28 21:04:55 +0300

    code: добавлен вывод наибольшего числа

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,4 +10,6 @@ int main()
     std::cout << "A * B = " << A * B << '\n';
     std::cout << "A / B = " << A / B << '\n';
     std::cout << "A % B = " << A % B << '\n';
+    auto max{ [](int a, int b) {if (a > b) return a; else return b; } };
+    std::cout << "Max: " << max(A, B);
 }
