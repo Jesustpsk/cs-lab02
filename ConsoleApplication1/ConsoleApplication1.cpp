016325ebc7783a7ecbda0e6da92e95a4f984ce39
bob (BobBB) <bob@example.com>
2023-06-28 21:07:03 +0300

    code: добавлен вывод минимального числа

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,4 +12,6 @@ int main()
     std::cout << "A % B = " << A % B << '\n';
     auto max{ [](int a, int b) {if (a > b) return a; else return b; } };
     std::cout << "Max: " << max(A, B);
+    auto min{ [](int a, int b) {if (a < b) return a; else return b; } };
+    std::cout << "Min: " << min(A, B);
 }
