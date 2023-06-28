c2d4efa1dd711a8957c19ea0e1407154401ea4de
Alice (IvanovII) <alice@example.com>
2023-06-28 21:34:36 +0300

    code: change type

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     std::cout << "Enter A and B: ";
-    int A, B;
+    double A, B;
     std::cin >> A >> B;
     std::cout << "A + B = " << A + B << '\n'
         << "A - B = " << A - B << '\n';
